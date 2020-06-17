0dff0cf624745cc2231c7ca25ed3c7cac620e1e4
demudu <avagaddademudubabu123@gmail.com>
2020-06-17 13:24:55 +0530

    print

--- add_cust.py
+++ add_cust.py
@@ -10,20 +10,21 @@ cur = cnx.cursor()
 class add:
     def insert_customer(self):
         name = input("enter customer name(max 10 letters):")
-        if len(name)!=10:
+        if len(name)>10:
           print("entered customer name is not in the above given order,please enter in the above given format")
-        cust_id = input("enter customer_id(it should be 6 letters starting with 'cu'':").lower()
+        cust_id = input("enter customer_id(it should be 6 letters starting with 'cu'):").lower()
         if len(cust_id)!=6 or cust_id[0]!='c' or cust_id[1]!='u':
           print("entered customer_id is not in the above given order,please enter in the above given format")
         cur.execute("select customer_id from customer2")
         a = cur.fetchall()
         for i in a:
-            if i != ' ':
-                if a != cust_id:
-                    print("entered customer_id is already in the list,please enter another customer_id")
-                    break
+          if i==cust_id:
+            print("entered customer_id is already there,please enter another customer_id")
+            break
         k = 0
         contact = int(input("enter phone number:"))
+        if contact!=10:
+          print("entered number is not a 10 digit number")
         cur.execute("insert into customer(custid,name,contact_details) values(?,?,?)", (cust_id, name, contact))
         cur.execute("insert into customer2(customer_name,customer_id)values(?,?)", name, cust_id, )
         cnx.commit()
