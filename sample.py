6aa6cddce49b13cfc177beac48abf343e1fa6c9a
demudu <avagaddademudubabu123@gmail.com>
2020-06-18 12:09:30 +0530

    print

--- sample.py
+++ sample.py
@@ -1,11 +1,33 @@
-import pyodbc
+import pyodbc as mc
 
-driver = 'SQL Server'
-server = 'DESKTOP-SFPRKLP\SQLEXPRESS'
-db1 = 'postpaid'
+cnx = mc.connect("Driver={SQL Server};"
+                 "Server=DESKTOP-SFPRKLP\SQLEXPRESS;"
+                 "Database=postpaid;"
+                 )
+cur = cnx.cursor()
 
-cnxn = pyodbc.connect(driver='{SQL Server}', host=server, database=db1)
-cursor = cnxn.cursor()
 
-# cnxn = pyodbc.connect(driver='{SQL Server}', host=server, database=db1,
-#                       trusted_connection=tcon, user=uname, password=pword)
+class add:
+    def insert_customer(self):
+        name = input("enter customer name(max 10 letters):")
+        cust_id = input("enter customer_id(it should be 6 letters starting with 'cu'):")
+        contact = int(input("enter the phone number:"))
+        cur.execute("select custid from customer")
+        f = cur.fetchall()
+        l = []
+        for i in range(len(f)):
+            l.append(f[i][i-1])
+        k = str(contact)
+        z = cur.fetchall()
+        for i in l:
+            if len(name) > 10 or len(cust_id) != 6 or cust_id[0] != 'c' or cust_id[1] != 'u' or i == cust_id or len(k) != 10:
+                print("you did not follow the above restrictions")
+                break
+        else:
+            cur.execute("insert into customer(custid,name,contact_details) values(?,?,?)", (cust_id, name, contact))
+            cur.execute("insert into customer2(customer_name,customer_id)values(?,?)", name, cust_id, )
+            cnx.commit()
+            print("\ncustomer details inserted successfully")
+
+
+a = add()
